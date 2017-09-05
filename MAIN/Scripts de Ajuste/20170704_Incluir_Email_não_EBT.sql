2340311651ebb5af939df986f1a0553ff2d4a905
Gilberto Almeida <galmeida@outlook.com>
2017-09-05 21:04:07 +0000

    git-tfs-id: [https://galmeida.visualstudio.com]$/EBT_Sislab;C592

--- MAIN/Scripts de Ajuste/20170704_Incluir_Email_não_EBT.sql
+++
@@ -1,5 +0,0 @@
-alter table agendamento add CONSTRAINT FK_Agendamento_UserCRT_RAT foreign key (AG_RAT) references UserCRT (Userid)
-go
-
-alter table agendamento add CONSTRAINT FK_Agendamento_UserCRT_RT foreign key (AG_RESPONSAVEL) references UserCRT (USERID)
-go
