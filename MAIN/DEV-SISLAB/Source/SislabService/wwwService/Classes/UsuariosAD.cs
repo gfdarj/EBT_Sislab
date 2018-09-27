569d027050a48afc371f14f60ce0b203723e3676
Gilberto Almeida <galmeida@outlook.com>
2018-09-27 21:06:21 +0000

    git-tfs-id: [https://galmeida.visualstudio.com]$/EBT_Sislab;C684

--- MAIN/DEV-SISLAB/Source/SislabService/wwwService/Classes/UsuariosAD.cs
+++ MAIN/DEV-SISLAB/Source/SislabService/wwwService/Classes/UsuariosAD.cs
@@ -79,11 +79,11 @@ namespace Embratel.Sislab.Classes
             XmlDocument xmldoc = new XmlDocument();
 
             //COM O CABEÇALHO XML
-            XmlNode xmlnode = xmldoc.CreateXmlDeclaration("1.0", "UTF-8", null);  //coloca o cabeçalho do XML
-            xmldoc.AppendChild(xmlnode);
+            //XmlNode xmlnode = xmldoc.CreateXmlDeclaration("1.0", "UTF-8", null);  //coloca o cabeçalho do XML
+            //xmldoc.AppendChild(xmlnode);
 
             //SEM O CABEÇALHO XML
-            //XmlNode xmlnode;   //retira o cabeçalho
+            XmlNode xmlnode;   //retira o cabeçalho
 
             //tag Documentos <Documentos>
             xmlnode = xmldoc.CreateElement("", "Usuario", "");
