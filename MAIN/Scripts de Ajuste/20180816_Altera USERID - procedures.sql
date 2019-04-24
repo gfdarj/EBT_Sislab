40b3f7dd1f3f46d1bd0a27884a1c6a638f4ecd47
Gilberto Almeida <galmeida@outlook.com>
2019-04-24 14:56:56 +0000

    Alterada a procedure SP_SCE_RECEBE_CARGA
    git-tfs-id: [https://galmeida.visualstudio.com]$/EBT_Sislab;C857

--- MAIN/Scripts de Ajuste/20180816_Altera USERID - procedures.sql
+++ MAIN/Scripts de Ajuste/20180816_Altera USERID - procedures.sql
@@ -6243,11 +6243,18 @@ BEGIN
 		de entrada na logistica, outra de saida em nome do outro usuario
 
 		COPPETEC: Gilberto Almeida
-		Criado: 12/11/2003	Ultima alteracao:
+		- Criado: 12/11/2003
+		- Ultima alteracao: 22/04/2019
+				Troquei o código para criar uma tabela temporária e "evitar" duplicação dos itens na lista de equipamentos pois
+				estava duplicando nos testes.
 	***/
 	SET NOCOUNT ON
 
 	BEGIN TRANSACTION
+
+	DECLARE @equip TABLE (
+		EQ_ID INT PRIMARY KEY
+	)
 
 	-- pega o nome do usuario e o codigo de barras
 	DECLARE @MSG VARCHAR(8000)
@@ -6256,7 +6263,7 @@ BEGIN
 		DECLARE @iini INT, @fim INT, @ifim INT
 		DECLARE @separa_registro VARCHAR(1), @reg VARCHAR(255)
 		DECLARE @eq_codigobarras VARCHAR(20)
-		DECLARE @resp_origem VARCHAR(80), @resp_destino VARCHAR(80)
+		DECLARE @resp_origem VARCHAR(20), @resp_destino VARCHAR(20)
 		DECLARE @ag_numero_orig INT, @hoje DATETIME
 
 		SET @hoje = GETDATE()
@@ -6265,16 +6272,35 @@ BEGIN
 		SET @eq_id = rtrim(ltrim( @eq_id ))
 		SET @fim = 0
 		SET @iini = 1
-		WHILE ( @fim = 0 ) BEGIN
+		WHILE ( @fim = 0 )
+		BEGIN
 			SET @ifim = PATINDEX('%' + @separa_registro + '%', @eq_id )
 			IF @ifim = 0 
 				SET @reg = SUBSTRING( @eq_id, @iini, LEN( @eq_id ) )
 			ELSE
 				SET @reg = SUBSTRING( @eq_id, @iini, @ifim - 1 )
 
+			IF NOT EXISTS(SELECT EQ_ID FROM @equip WHERE EQ_ID = CAST(@reg AS INT))
+			BEGIN
+				INSERT INTO @equip (EQ_ID) VALUES(CAST(@reg AS INT))
+			END
+
+			SET @eq_id = LTRIM(SUBSTRING( @eq_id, @ifim + 1, LEN( @eq_id ) ))
+			IF @ifim = 0 SET @fim = 1
+		END
+
+		DECLARE @eq INT
+		DECLARE curEQUIP CURSOR LOCAL FORWARD_ONLY
+			 FOR
+				SELECT EQ_ID FROM @equip
+		OPEN curEQUIP
+		FETCH NEXT FROM curEQUIP INTO @eq
+		WHILE @@FETCH_STATUS = 0
+		BEGIN
+
 			UPDATE SCE_Passagem_Carga 
-				SET PAS_APROVADO = 1, PAS_DATARECEBIMENTO = GETDATE()
-				WHERE AG_NUMERO_DEST = @ag_numero_dest AND EQ_ID = CAST(@reg AS INT)
+				SET PAS_APROVADO = 1, PAS_DATARECEBIMENTO = @hoje
+				WHERE AG_NUMERO_DEST = @ag_numero_dest AND EQ_ID = @eq
 			IF @@ERROR <> 0 BEGIN
 				ROLLBACK TRANSACTION
 				RAISERROR( 'Não foi possível realizar o recebimento de carga.', 16, 1)
@@ -6283,17 +6309,20 @@ BEGIN
 
 			-- responsavel do ag de origem
 			SELECT @resp_origem = a.AG_RESPONSAVEL, @ag_numero_orig = a.AG_NUMERO
-				FROM Agendamento a INNER JOIN
-				SCE_Passagem_Carga p ON a.AG_NUMERO = p.AG_NUMERO_ORIG
-				WHERE AG_NUMERO_DEST = @ag_numero_dest AND p.EQ_ID = @reg
+				FROM Agendamento a 
+				INNER JOIN SCE_Passagem_Carga p ON a.AG_NUMERO = p.AG_NUMERO_ORIG
+				WHERE 
+					AG_NUMERO_DEST = @ag_numero_dest AND p.EQ_ID = @eq
 
 			-- responsavel do ag de destino
-			SELECT @resp_destino = a.AG_RESPONSAVEL FROM Agendamento a INNER JOIN
-				SCE_Passagem_Carga p ON a.AG_NUMERO = p.AG_NUMERO_DEST
-				WHERE AG_NUMERO_DEST = @ag_numero_dest AND p.EQ_ID = @reg
+			SELECT @resp_destino = a.AG_RESPONSAVEL 
+				FROM Agendamento a 
+				INNER JOIN SCE_Passagem_Carga p ON a.AG_NUMERO = p.AG_NUMERO_DEST
+				WHERE 
+					AG_NUMERO_DEST = @ag_numero_dest AND p.EQ_ID = @eq
 
 			-- entrada log
-			EXEC sp_SCE_CADASTRA_MOVIMENTACAO NULL, @reg, @hoje, 525 /*LAB - Devolução de teste no laboratório*/, @user_id, @resp_origem, 2 /*Entrada Log*/, NULL, @ag_numero_orig, 0, NULL, NULL, 1, NULL /* Localizacao do item */, null, null, @user_id
+			EXEC sp_SCE_CADASTRA_MOVIMENTACAO NULL, @eq, @hoje, 525 /*LAB - Devolução de teste no laboratório*/, @user_id, @resp_origem, 2 /*Entrada Log*/, NULL, @ag_numero_orig, 0, NULL, NULL, 1, NULL /* Localizacao do item */, NULL, NULL, @user_id
 			IF @@ERROR <> 0 BEGIN
 				ROLLBACK TRANSACTION
 				RAISERROR( 'Não foi possível movimentar a entrada dos itens.', 16, 1)
@@ -6301,16 +6330,15 @@ BEGIN
 			END
 
 			-- saida log
-			EXEC sp_SCE_CADASTRA_MOVIMENTACAO NULL, @reg, @hoje, 524 /*LAB - Saída para teste c/ AS no laboratório*/, @user_id, @resp_destino, 4 /*Saida Log*/, NULL, @ag_numero_dest, 0, NULL, NULL, 1, NULL /* Localizacao do item */, null, null, @user_id
+			EXEC sp_SCE_CADASTRA_MOVIMENTACAO NULL, @eq, @hoje, 524 /*LAB - Saída para teste c/ AS no laboratório*/, @user_id, @resp_destino, 4 /*Saida Log*/, NULL, @ag_numero_dest, 0, NULL, NULL, 1, NULL /* Localizacao do item */, NULL, NULL, @user_id
 			IF @@ERROR <> 0 BEGIN
 				ROLLBACK TRANSACTION
 				RAISERROR( 'Não foi possível movimentar a saída dos itens.', 16, 1)
 				RETURN -1
 			END
 
-
 			-- pega o nome do usuario e o codigo de barras
-			SELECT @eq_codigobarras = EQ_CODIGOBARRAS FROM SCE_Equipamentos WHERE EQ_ID = @reg
+			SELECT @eq_codigobarras = EQ_CODIGOBARRAS FROM SCE_Equipamentos WHERE EQ_ID = @eq
 
 			SET @MSG = 'O usuário ' + @user_id + ' recebeu a carga do equipamento ' + @eq_codigobarras
 
@@ -6321,14 +6349,16 @@ BEGIN
 				RAISERROR( 'Não foi possível inserir no histórico', 16, 1)
 				RETURN -1
 			END
-			--
 
-			SET @eq_id = LTRIM(SUBSTRING( @eq_id, @ifim + 1, LEN( @eq_id ) ))
-			IF @ifim = 0 SET @fim = 1
-		END
-	END
+			FETCH NEXT FROM curEQUIP INTO @eq
+		END -- FETCH
+
+		CLOSE curEQUIP
+		DEALLOCATE curEQUIP
+    END
 
 	COMMIT TRANSACTION
 	RETURN 1
 END
 GO
+
