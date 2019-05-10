487fd6923cebe566f52c1c8e06744002f05523d7
Gilberto Almeida <galmeida@outlook.com>
2019-05-10 19:22:52 +0000

    git-tfs-id: [https://galmeida.visualstudio.com]$/EBT_Sislab;C865

--- MAIN/Scripts de Ajuste/20180816_Altera USERID - procedures.sql
+++ MAIN/Scripts de Ajuste/20180816_Altera USERID - procedures.sql
@@ -6362,3 +6362,39 @@ BEGIN
 END
 GO
 
+
+/****** Object:  View [dbo].[vw_Agendamento]    Script Date: 07/19/2018 18:45:13 ******/
+IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[vw_Agendamento]') AND OBJECTPROPERTY(id, N'IsView') = 1)
+	DROP VIEW [dbo].[vw_Agendamento]
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER OFF
+GO
+
+CREATE VIEW [dbo].[vw_Agendamento]
+AS
+	/* Exibe os dados do agendamento e a sua situacao atual */
+	SELECT 
+		a.*, h.HE_RESPONSAVEL, h.HE_DATAINICIO, h.HE_DATATERMINO, h.HE_MOTIVO, 
+		s.ID_SITUACAO, s.S_DESCRICAO, ta.TA_DESCRICAO, t.TEC_NOME, ts.TS_DESCRICAO
+	FROM 
+		Agendamento a 
+		/*** Pega o status pela maior Data ***/
+		INNER JOIN Historico_Eventos h ON a.AG_NUMERO = h.AG_NUMERO
+		INNER JOIN (
+			SELECT h1.AG_NUMERO, MAX(h1.HE_ID) AS HE_ID
+			FROM Historico_Eventos h1
+				INNER JOIN (
+					SELECT AG_NUMERO, MAX(HE_DATAINICIO) AS HE_DATAINICIO 
+					FROM Historico_Eventos hh1
+					--WHERE hh1.AG_NUMERO = h.AG_NUMERO
+					GROUP BY hh1.AG_NUMERO
+				) h2 ON h1.AG_NUMERO = h2.AG_NUMERO AND h1.HE_DATAINICIO = h2.HE_DATAINICIO
+			GROUP BY h1.AG_NUMERO
+		) h3 ON h3.HE_ID = h.HE_ID
+		INNER JOIN Situacoes s ON h.ID_SITUACAO = s.ID_SITUACAO
+		LEFT JOIN Tipo_Atividade ta ON ta.TA_ID = a.TA_ID
+		LEFT JOIN Tecnologia t ON t.TEC_ID = a.TEC_ID
+		LEFT JOIN Tipo_Sigilo ts on ts.TS_ID = a.AG_SIGILO
+GO
