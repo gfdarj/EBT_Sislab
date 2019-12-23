bdf79c6cc78e4bad7ec138cc1dcdfb4a855dc6e1
Gilberto Almeida <galmeida@outlook.com>
2019-12-23 19:30:16 +0000

    Alterações no menu do sislab antigo para torná-lo dinamico
    git-tfs-id: [https://galmeida.visualstudio.com]$/EBT_Sislab;C958

---
+++ MAIN/Scripts de Ajuste/Migração 2019/05-Criados 2 tipos de arquivos.sql
@@ -0,0 +1,9 @@
+-- OBS: já executado em 23/12/2019
+
+--id 35
+insert into tipoArquivo values ('Conhecendo o CRT', 0, null, 0)
+go
+
+--id 36
+insert into tipoArquivo values ('Recursos Disponíveis do CRT', 0, null, 0)
+go
