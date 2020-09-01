76bd29dd391f119e028abd855e85c20b49febe47
Gilberto Almeida <galmeida@outlook.com>
2020-09-01 20:07:53 +0000

    git-tfs-id: [https://galmeida.visualstudio.com]$/EBT_Sislab;C1008

--- MAIN/Scripts de Ajuste/Migração 2019/02-Altera USERID - procedures.sql
+++ MAIN/Scripts de Ajuste/Migração 2019/02-Altera USERID - procedures.sql
@@ -1757,60 +1757,6 @@ END
 GO
 
 
-/****** Object:  StoredProcedure [dbo].[sp_CadAmbiente]    Script Date: 07/19/2018 18:45:13 ******/
-IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[sp_CadAmbiente]') AND OBJECTPROPERTY(id,N'IsProcedure') = 1)
-	DROP PROCEDURE [dbo].[sp_CadAmbiente]
-GO
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-
-CREATE PROCEDURE [dbo].[sp_CadAmbiente]
-(
-	@pId INT,
-	@pDescricao VARCHAR(510),
-	@pUsadoPorAg BIT,
-	@pModulo TINYINT,
-	@pCRT TINYINT
-)
-AS
-BEGIN
-	BEGIN TRANSACTION
-	SET NOCOUNT ON 
-
-	DECLARE @msg VARCHAR(8000)
-
-	IF (@pID IS NULL) OR (@pID = 0) BEGIN
-		INSERT INTO Ambientes (AMB_NOME, AMB_USADOPORAG, AMB_MODULO, ID_CRT) VALUES (@pDescricao, @pUsadoPorAg, @pModulo, @pCRT)
-		IF @@ERROR <> 0 BEGIN
-			ROLLBACK TRANSACTION
-			SET @msg = 'Não foi possível inserir o ambiente ' + @pDescricao
-			RAISERROR(@msg, 16, 1)
-			SELECT -1 AS SAIDA, @msg AS MENSAGEM
-			RETURN -1
-		END
-		SET @pID = @@IDENTITY
-	END
-	ELSE BEGIN
-		UPDATE	Ambientes 
-			SET AMB_NOME = @pDescricao, AMB_USADOPORAG = @pUsadoPorAg, AMB_MODULO = @pModulo, ID_CRT = @pCRT
-			WHERE	AMB_ID = @pId
-		IF @@ERROR <> 0 BEGIN
-			ROLLBACK TRANSACTION
-			SET @msg = 'Não foi possível atualizar o ambiente ' + @pDescricao
-			RAISERROR(@msg, 16, 1)
-			SELECT -1 AS SAIDA, @msg AS MENSAGEM
-			RETURN -1
-		END
-	END
-
-	COMMIT TRANSACTION
-	SELECT @pID AS SAIDA, 'OK' AS MENSAGEM
-	RETURN @pID
-END
-GO
-
 /****** Object:  StoredProcedure [dbo].[sp_CadAreaTecnologica]    Script Date: 07/19/2018 18:45:13 ******/
 IF  EXISTS (SELECT * FROM dbo.sysobjects WHERE id = OBJECT_ID(N'[dbo].[sp_CadAreaTecnologica]') AND OBJECTPROPERTY(id,N'IsProcedure') = 1)
 	DROP PROCEDURE [dbo].[sp_CadAreaTecnologica]
